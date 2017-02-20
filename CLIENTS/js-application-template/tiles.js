f7b6b3f9b8bfeeb19f98e45e4fa58e3dae920f36
andersri <anderrma@stud.ntnu.no>
2017-02-20 15:16:49 +0100

    Adding ipAddressHolder

--- CLIENTS/js-application-template/tiles.js
+++ CLIENTS/js-application-template/tiles.js
@@ -1,6 +1,6 @@
 var tilesLib = require('/tiles-lib');
 
-var client = new tilesLib.TilesClient('Anders', '138.68.144.206', 1883).setApplication("{{appNameHolder}}").connect();
+var client = new tilesLib.TilesClient('Anders', '{{ipAddressHolder}}', 1883).setApplication("{{appNameHolder}}").connect();
 var reader = new tilesLib.EventReader();
 // var PostmanClient = new tilesLib.PostmanClient('192.168.1.6', 3000);
 // var IFTTTClient = new tilesLib.IFTTTClient('dncBS7n2aVGR4Bf44Yx9Ck');
