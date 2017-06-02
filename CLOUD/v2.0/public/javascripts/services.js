fb5536934afa65ee8a5eb34a00a8c48cc5d5b7eb
andersri <anderrma@stud.ntnu.no>
2017-06-02 21:07:20 +0200

    adding varun and jonas to copyright

--- CLOUD/v2.0/public/javascripts/services.js
+++ CLOUD/v2.0/public/javascripts/services.js
@@ -1,5 +1,5 @@
 /*
-   Copyright 2017 Anders Riise Mæhlum
+   Copyright 2017 Anders Riise Mæhlum, Varun Sivapalan & Jonas Kirkemyr
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
